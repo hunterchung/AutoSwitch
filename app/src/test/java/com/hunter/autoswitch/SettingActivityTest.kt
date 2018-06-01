8defc631bccaa3e23b67c2799681a3ad39f2b41f
Hunter Chung <b89603112@gmail.com>
2018-05-31 20:53:12 -0700

    Initial commit

---
+++ app/src/test/java/com/hunter/autoswitch/SettingActivityTest.kt
@@ -0,0 +1,15 @@
+package com.hunter.autoswitch
+
+import android.database.MatrixCursor
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SettingActivityTest {
+
+    @Test
+    fun onOptionsItemSelected() {
+        val cursor = MatrixCursor(arrayOf("a"))
+        println(cursor)
+    }
+}
